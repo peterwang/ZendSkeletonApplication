03bbbdbb051ff8db15c948471d21fcef0ac5b8c8
Peter Wang <peterwang@vip.qq.com>
2012-12-08 11:29:31 +0100

    some config tuning.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; -*- mode:emacs-lisp; -*-
+((nil . ((indent-tabs-mode . t)
+	 (fill-column . 80)))
+ (php-mode . ((indent-tabs-mode . nil) ; insert space when indent in php mode
+	      (fill-column . 80)))
+ (subdirs . t))
