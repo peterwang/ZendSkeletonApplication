4e8eb831951874086c59880154b9af6e57cef300
Peter Wang <peterwang@vip.qq.com>
2012-12-08 17:38:30 +0100

    build tags for emacs.

---
+++ build_ctags.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cwd=`cd $(dirname $0);pwd`
+cd $cwd;
+
+listfile=$cwd/myapp1.LIST
+tagsfile=$cwd/myapp1.TAGS
+
+find $cwd -iname '*\.php' -type f|grep -v ZendTest > $listfile
+
+/usr/local/bin/ctags -e -R \
+    -L $listfile \
+    -h ".php" \
+    -f $tagsfile \
+    --exclude=.git \
+    --exclude=ZendTest \
+    --totals=yes \
+    --tag-relative=yes \
+    --PHP-kinds=+cf-v \
+    --regex-PHP='/abstract\s+class\s+([^ ]+)/\1/c/' \
+    --regex-PHP='/interface\s+([^ ]+)/\1/c/' \
+    --regex-PHP='/(public\s+|static\s+|abstract\s+|protected\s+|private\s+)function\s+\&?\s*([^ (]+)/\2/f/'
